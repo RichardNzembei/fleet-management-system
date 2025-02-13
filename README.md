e9ca13472b8ed38d756dfa555ff63e26ec16d111
RichardNzembei <richardsonreuben78@gmail.com>
2025-02-13 12:46:07 +0300

    delete 129 files

--- README.md
+++
@@ -1,2 +0,0 @@
-# currency_value_chain_app
-
--- android/app/src/main/kotlin/com/example/currency_value_chain_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.currency_value_chain_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
